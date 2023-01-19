619f56f632fbf2e4cf3f1d666e98b6d0c4664fba
Egor Kolesnikov <freethinkel@gmail.com>
2023-01-19 21:59:28 +0600

    fix build

--- src-tauri/src/patch_window/mod.rs
+++ src-tauri/src/patch_window/mod.rs
@@ -7,17 +7,9 @@ use tauri::Window as TauriWindow;
 pub trait PatchWindow {
     #[cfg(target_os = "macos")]
     fn apply_toolbar(&self, tickness: ToolbarThickness);
-
-    #[cfg(target_os = "macos")]
-    fn apply_fix_blur(&self);
 }
 
 impl PatchWindow for TauriWindow {
-    #[cfg(target_os = "macos")]
-    fn apply_fix_blur(&self) {
-        macos::apply_fix_blur(self.ns_window().unwrap() as _);
-    }
-
     #[cfg(target_os = "macos")]
     fn apply_toolbar(&self, tickness: ToolbarThickness) {
         macos::apply_toolbar(self, tickness);
